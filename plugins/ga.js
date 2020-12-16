41c522ccdb282215a8b131e19861189d412c2dc9
mohammednadeem0918 <mohammed.nadeem918@outlook.com>
2020-12-17 02:34:51 +0700

    defer is added

--- plugins/ga.js
+++ plugins/ga.js
@@ -3,16 +3,14 @@ export default ({ app }) => {
    ** Only run on client-side and only in production mode
    */
   if (process.env.NODE_ENV !== 'production') {
-    return /*
-     ** Include Google Analytics Script
-     */
+    return
   }(function (w, d, s, l, i) {
     w[l] = w[l] || []; w[l].push({
       'gtm.start':
       new Date().getTime(),
       event: 'gtm.js'
     }); const f = d.getElementsByTagName(s)[0]
-    const j = d.createElement(s); const dl = l !== 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+    const j = d.createElement(s); const dl = l !== 'dataLayer' ? '&l=' + l : ''; j.async = true; j.defer = true; j.src =
     'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f)
   })(window, document, 'script', 'dataLayer', 'GTM-W7VNK38')
 }
