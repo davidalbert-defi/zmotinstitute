bc42705e9dcbaa4b25962a5f1f62402173825fb5
nick <nick@perceptively.com>
2021-05-18 17:00:27 -0700

    hubspot form fix v2

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,95 +1,87 @@
 <template>
-  <section
+    <section
       class="leadform py-6"
       id="contactform">
-    <div class="relative bg-orange-100 lg:m-12 rounded-3xl">
-      <div class="absolute inset-0 ">
-        <div class="absolute inset-y-0 left-0 w-1/2"></div>
-      </div>
-      <div class="relative max-w-7xl lg:grid lg:grid-cols-5">
-        <div class="bg-gray-50 py-12 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
-          <div class="max-w-lg mx-auto">
-            <h2 class="text-4xl lg:text-5xl font-bold tracking-tight text-orange-shinny text-center">
-              {{ $t('contact_form.lead_form_headline') }}
-            </h2>
-            <p class="mt-12 text-xl leading-6 text-orange-shinny font-normal">
-              {{ $t('contact_form.lead_form_sub_headline_1') }}
-            </p>
-            <p class="mt-12 text-xl leading-6 text-orange-shinny font-normal">
-              {{ $t('contact_form.lead_form_sub_headline_2') }}
-            </p>
+      <div class="relative bg-orange-100 lg:m-12 rounded-3xl">
+        <div class="relative max-w-7xl lg:grid lg:grid-cols-5">
+          <div class="bg-gray-50 py-12 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
+            <div class="max-w-lg mx-auto">
+              <h2 class="text-4xl lg:text-5xl font-bold tracking-tight text-orange-shinny text-center">
+                {{ $t('contact_form.lead_form_headline') }}
+              </h2>
+              <p class="mt-12 text-xl leading-6 text-orange-shinny font-normal">
+                {{ $t('contact_form.lead_form_sub_headline_1') }}
+              </p>
+              <p class="mt-12 text-xl leading-6 text-orange-shinny font-normal">
+                {{ $t('contact_form.lead_form_sub_headline_2') }}
+              </p>
+            </div>
           </div>
-        </div>
-        <div class="py-2 px-4 sm:px-6 lg:col-span-3 lg:pt-24 lg:px-8 xl:pl-12">
-          <div class="max-w-lg mx-auto lg:max-w-none">
-            <form class="grid grid-cols-1 gap-y-6" @submit.prevent="onSubmit">
+          <div class="py-2 px-4 sm:px-6 lg:col-span-3 lg:pt-24 lg:px-8 xl:pl-12">
+            <form class="w-full" @submit.prevent="onSubmit">
               <div>
-                <label for="full_name" class="sr-only">{{ $t('contact_form.lead_form.field_name') }}</label>
-                <input
-                  type="text"
-                  name="full_name"
-                  id="full_name"
-                  autocomplete="name"
-                  v-model="name"
-                  class="bg-orange-100 text-xl block w-full shadow-inner py-3 px-4 placeholder-orange-shinny focus:ring-orange-500 focus:border-orange-500 border-orange-700 rounded-md m-0"
-                  :placeholder="`${$t('contact_form.lead_form.field_name')}`">
-              </div>
-              <div>
-                <label for="email" class="sr-only">{{ $t('contact_form.lead_form.field_email') }}</label>
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autocomplete="email"
-                  v-model="email"
-                  class=" bg-orange-100 text-xl block w-full shadow-inner py-3 px-4 placeholder-orange-shinny focus:ring-orange-500 focus:border-orange-500 border-orange-300 rounded-md m-0"
-                  :placeholder="`${$t('contact_form.lead_form.field_email')}`">
-              </div>
-              <div>
-                <label for="phone" class="sr-only">{{ $t('contact_form.lead_form.field_phone') }}</label>
-                <input
-                type="text"
-                name="phone"
-                id="phone"
-                autocomplete="tel"
-                v-model="phone"
-                class=" bg-orange-100 text-xl block w-full shadow-inner py-3 px-4 placeholder-orange-shinny focus:ring-orange-500 focus:border-orange-500 border-orange-300 rounded-md m-0"
-                :placeholder="`${$t('contact_form.lead_form.field_phone')}`">
-              </div>
-              <div>
-                <label for="message" class="sr-only">{{ $t('contact_form.lead_form.field_message') }}</label>
-                <textarea
-                  id="message"
-                  name="message"
-                  rows="4"
-                  v-model="message"
-                  class="bg-orange-100 text-xl block w-full shadow-inner py-3 px-4 placeholder-orange-shinny focus:ring-orange-500 focus:border-orange-500 border-gray-300 rounded-md m-0"
-                  :placeholder="`${$t('contact_form.lead_form.field_message')}`"></textarea>
-              </div>
-              <recaptcha
+                <div>
+                  <label for="email" class="sr-only">{{ $t('contact_form.lead_form.field_email') }}</label>
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autocomplete="email"
+                    v-model="email"
+                    class=" bg-orange-100 text-xl block w-full shadow-inner py-3 px-4 placeholder-orange-shinny focus:ring-orange-500 focus:border-orange-500 border-orange-300 rounded-md m-0"
+                    :placeholder="`${$t('contact_form.lead_form.field_email')}`">
+                </div>
+                <div class="mt-6">
+                <label for="name" class="sr-only">{{ $t('contact_form.lead_form.field_name') }}</label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    autocomplete="name"
+                    v-model="name"
+                    class="bg-orange-100 text-xl block w-full shadow-inner py-3 px-4 placeholder-orange-shinny focus:ring-orange-500 focus:border-orange-500 border-orange-700 rounded-md m-0"
+                    :placeholder="`${$t('contact_form.lead_form.field_name')}`">
+                </div>
+                <div class="mt-6 mb-6">
+                  <label for="surname" class="sr-only">{{ $t('contact_form.lead_form.field_name') }}</label>
+                  <input
+                    type="text"
+                    name="surname"
+                    id="surname"
+                    autocomplete="surname"
+                    v-model="surname"
+                    class="bg-orange-100 text-xl block w-full shadow-inner py-3 px-4 placeholder-orange-shinny focus:ring-orange-500 focus:border-orange-500 border-orange-700 rounded-md m-0"
+                    :placeholder="`${$t('contact_form.lead_form.field_name')}`">
+                </div>
+                <client-only>
+                  <vue-tel-input v-model="phone" v-bind="telInputOption" required :placeholder="$t('placeholder.phone')" @country-changed="countryChanged"></vue-tel-input>
+                </client-only>
+                <recaptcha
                   @error="onError"
                   @success="onSuccess"
                   @expired="onExpired"
                 />
-              <div class="mx-auto pb-6">
-                <button
-                  type="submit"
-                  class="inline-flex font-semibold justify-center py-3 px-6  border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-green-shinny hover:bg-green-shinny focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                  {{ $t('contact_form.lead_form.submit') }}
-                </button>
-                <p class="mt-6 text-sm text-orange-shinny">
-                  {{ $t('contact_form.lead_form.privacy_1') }}
-                  <a href="/privacy" class="text-orange-shinny font-medium underline">
-                  {{ $t('contact_form.lead_form.privacy_2') }}
-                  </a>
-                </p>
+                <div class="mx-auto pb-6">
+                  <button
+                    id="SubmitLeadForm"
+                    type="submit"
+                    class="inline-flex font-semibold justify-center py-3 px-6  border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-green-shinny hover:bg-green-shinny focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    {{ $t('contact_form.lead_form.submit') }}
+                  </button>
+                  <p class="mt-6 text-sm text-orange-shinny">
+                    {{ $t('contact_form.lead_form.privacy_1') }}
+                    <a href="/privacy" class="text-orange-shinny font-medium underline">
+                    {{ $t('contact_form.lead_form.privacy_2') }}
+                    </a>
+                  </p>
+                </div>
               </div>
             </form>
           </div>
         </div>
       </div>
-    </div>
-    <div v-if="submitting" class="flex content-center justify-center min-h-screen w-full z-100 absolute top-0 left-0 bg-white bg-opacity-30 pt-45vh">
+      <div v-if="submitting" class="flex content-center justify-center min-h-screen w-full z-100 absolute top-0 left-0 bg-white bg-opacity-30 pt-45vh">
       <loading
         :active.sync="submitting"
         :can-cancel="false"
@@ -97,19 +89,31 @@
         color="white"
       />
     </div>
-  </section>
+    </section>
 </template>
+
 <script>
+import { VueTelInput } from 'vue-tel-input'
+import { mapGetters } from 'vuex'
 export default {
-  name: 'ContactForm',
+  name: 'SectionZmotAudit',
+  components: { VueTelInput },
   data: () => ({
-    name: '',
     email: '',
+    name: '',
+    surname: '',
     phone: '',
-    message: '',
+    country: '',
     submitting: false
   }),
+  computed: {
+    ...mapGetters(['telInputOption'])
+  },
   methods: {
+    countryChanged (country) {
+      this.country = country.dialCode
+      this.phone = '+' + country.dialCode + ' '
+    },
     async onSubmit () {
       try {
         const token = await this.$recaptcha.getResponse()
@@ -121,20 +125,24 @@ export default {
         const data = {
           fields: [
             {
-              name: 'full_name',
+              name: 'email',
+              value: this.email
+            },
+            {
+              name: 'firstname',
               value: this.name
             },
             {
-              name: 'email',
-              value: this.email
+              name: 'lastname',
+              value: this.surname
             },
             {
               name: 'phone',
               value: this.phone
             },
             {
-              name: 'message',
-              value: this.message
+              name: 'service_interest',
+              value: 'online-courses'
             },
             {
               name: 'form_url',
@@ -197,5 +205,4 @@ export default {
     }
   }
 }
-
 </script>
